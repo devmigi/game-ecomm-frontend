0f98602a39bcffff0e34ddac3b6e44bdee49db3c
Mrigendra <mrgndrakr@live.com>
2020-09-16 22:55:48 +0530

    home header added

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,24 +1,60 @@
 <nav class="flex items-center justify-between flex-wrap bg-gray-100 py-4 px-6" x-data="{isOpen : false}">
     <div class="flex items-center flex-shrink-0 text-gray mr-6">
-        <img src="/images/logo-text.png" class="h-6" alt="Gamelelo">
+        <img src="/images/logo-text.png" class="h-6 md:h-8" alt="Gamelelo">
     </div>
 
-    <div class="flex-1 items-start">
-        <input type="text" class="rounded bg-gray-200 px-4 py-3 text-xs w-1/2 mx-auto" placeholder="Search Games, Consoles and more..">
+
+    <div class="relative flex-1 flex text-gray-600 focus-within:text-gray-400 lg:max-w-2xl lg:mt-0 mt-4 order-last lg:order-none">
+        <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+            <button class="p-1 focus:outline-none focus:shadow-outline">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6">
+                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              </svg>
+            </button>
+        </span>
+        <input type="text" name="query" class="py-3 flex-1 text-sm text-white bg-white rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-700" placeholder="Search Games, Consoles and more.." autocomplete="off">
     </div>
 
-    <div >
-        <a href="#" class="p-1 mr-4">
+
+
+    <div class="top-menu text-gray-300  ">
+        <a href="tel:+91-9900990011" class="top-menu-link">
+            <svg width="1.1em" height="1.1em" viewBox="0 0 16 16" class="bi bi-gift" fill="currentColor" style="position:relative; top:-3px" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 14.5V7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A2.968 2.968 0 0 1 3 2.506V2.5zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43a.522.522 0 0 0 .023.07zM9 3h2.932a.56.56 0 0 0 .023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0V3zM1 4v2h6V4H1zm8 0v2h6V4H9zm5 3H9v8h4.5a.5.5 0 0 0 .5-.5V7zm-7 8V7H2v7.5a.5.5 0 0 0 .5.5H7z"/>
+            </svg>
+            <span class="hidden md:inline">Offers</span>
+        </a>
+        |
+        <a href="" class="top-menu-link">
+            <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor" style="position:relative; top:-2px" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M12.166 8.94C12.696 7.867 13 6.862 13 6A5 5 0 0 0 3 6c0 .862.305 1.867.834 2.94.524 1.062 1.234 2.12 1.96 3.07A31.481 31.481 0 0 0 8 14.58l.208-.22a31.493 31.493 0 0 0 1.998-2.35c.726-.95 1.436-2.008 1.96-3.07zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                <path fill-rule="evenodd" d="M8 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+            </svg>
+            <span class="hidden md:inline">Store</span>
+        </a>
+        |
+        <a href="tel:+91-9900990011" class="top-menu-link">
+            <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-headset" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 1a5 5 0 0 0-5 5v4.5H2V6a6 6 0 1 1 12 0v4.5h-1V6a5 5 0 0 0-5-5z"/>
+                <path d="M11 8a1 1 0 0 1 1-1h2v4a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V8zM5 8a1 1 0 0 0-1-1H2v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V8z"/>
+                <path fill-rule="evenodd" d="M13.5 8.5a.5.5 0 0 1 .5.5v3a2.5 2.5 0 0 1-2.5 2.5H8a.5.5 0 0 1 0-1h3.5A1.5 1.5 0 0 0 13 12V9a.5.5 0 0 1 .5-.5z"/>
+                <path d="M6.5 14a1 1 0 0 1 1-1h1a1 1 0 1 1 0 2h-1a1 1 0 0 1-1-1z"/>
+            </svg>
+            <span class="hidden md:inline">Support</span>
+        </a>
+        |
+        <a href="#" class="top-menu-link">
             <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-person-lines-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7 1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm2 9a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
             </svg>
-            <span class="hidden sm:inline">Profile</span>
+            <span class="hidden md:inline">Profile</span>
         </a>
-        <a href="#" class="p-1 ">
+        |
+        <a href="#" class="top-menu-link">
             <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-cart3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
             </svg>
-            <span class="hidden sm:inline">Cart</span>
+            <span class="hidden md:inline">Cart</span>
         </a>
     </div>
 </nav>
